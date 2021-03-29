654a32b775096ba583adc4d5ca9e195ab57364c7
tskarhed <1438972+tskarhed@users.noreply.github.com>
2021-03-29 12:12:27 +0200

    Correcting the function

--- resistance.jl
+++ resistance.jl
@@ -13,7 +13,7 @@ md"""
 
 The problem is setup as a rectangle with width 2L and height 2b. In the center there is a hole with radius a. The conductivtiy of the material is $\sigma$. a will be varied.
 
-$ R = \frac{b}{\sigma L}\frac{b(L^2+a^2)}{b^2-a^2} $
+$ R = \frac{b}{\sigma L}\frac{L^2+a^2}{b^2-a^2} $
 """
 
 # ╔═╡ a6e8f7a0-906e-11eb-1a0e-51f4a4b751f7
@@ -26,16 +26,16 @@ b = 30;
 L = 50;
 
 # ╔═╡ de4fdbf2-906e-11eb-2715-6f5bc8d68d2a
-a = 1:b/2;
+a = 0.01:0.1:10;
 
 # ╔═╡ 662d3360-906f-11eb-2826-d18803f2c32e
-R = (b^2*(L^2 .+ a.^2))/(σ*L*(b^2 .- a.^2))
+R = (b*(L^2 .+ a.^2))/(σ*L*(b^2 .- a.^2))
 
 # ╔═╡ 3c5e1dee-9070-11eb-1369-6509d6df4f3c
 plot(a, R[:,1], xlabel="a [m]", ylabel="Resistance [Ohms]", label="R")
 
 # ╔═╡ Cell order:
-# ╠═1b7aa6b0-906d-11eb-1bcd-9fd48415fade
+# ╟─1b7aa6b0-906d-11eb-1bcd-9fd48415fade
 # ╠═04af0d10-9070-11eb-2fbf-493475b83bd0
 # ╠═a6e8f7a0-906e-11eb-1a0e-51f4a4b751f7
 # ╠═d3572c80-906e-11eb-0a39-73a83bffb283
